ee458768a55a23c3dd5ad27447d5ec582b4c8029
Peter <github@peter.dev.br>
2023-03-26 15:47:32 -0300

    refactor(mobile): styles in QuizInput

--- apps/mobile/src/app/components/QuizInput.tsx
+++ apps/mobile/src/app/components/QuizInput.tsx
@@ -1,20 +1,17 @@
-import { forwardRef } from 'react';
-import {
-  Button,
-  StyleSheet,
-  TextInput,
-  TextInputProps,
-  View,
-} from 'react-native';
-import styles from './QuizFormControl.style';
+import { StyleSheet, TextInput, TextInputProps, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 type QuizInputProps = TextInputProps & {
   onClear?: () => void;
+  showClearBtn?: boolean;
 };
+
+// TODO: usar adornments
 
 export default function QuizInput({
   style,
   onClear,
+  showClearBtn = true,
   ...inputProps
 }: QuizInputProps) {
   return (
@@ -25,7 +22,19 @@ export default function QuizInput({
         blurOnSubmit={false}
       />
       <View style={inputStyle.clear}>
-        <Button title="clear" onPress={onClear}></Button>
+        {showClearBtn && (
+          <MaterialIcons.Button
+            name="clear-all"
+            backgroundColor="unset"
+            color="black"
+            onPress={onClear}
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            size={24}
+          ></MaterialIcons.Button>
+        )}
       </View>
     </View>
   );
@@ -35,18 +44,21 @@ const inputStyle = StyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   clear: {
     position: 'absolute',
     right: 12,
-    top: '20%',
   },
   input: {
+    outline: 'none',
+    outlineColor: 'transparent',
+    underlineColorAndroid: 'transparent',
     borderColor: '#BDBDBD',
     borderRadius: 12,
     borderWidth: 2,
     padding: 16,
-    paddingRight: 74,
+    paddingRight: 60,
     fontSize: 16,
   },
 });
