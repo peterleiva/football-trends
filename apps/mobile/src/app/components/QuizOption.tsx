4f14eb8c3a83afe3bc99c5c2c012e4c93d099b26
Peter <github@peter.dev.br>
2023-03-27 21:17:24 -0300

    feat(mobile): quiz option component

--- apps/mobile/src/app/components/QuizOption.tsx
+++ apps/mobile/src/app/components/QuizOption.tsx
@@ -1,9 +1,54 @@
-import { View, Text } from 'react-native';
+import { capitalize } from '@utils';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import Divider from './Divider';
 
-export default function QuizOption() {
+interface QuizOptionProps {
+  /**
+   * Number relative relative to other options
+   */
+  order: number;
+  /**
+   * Text describing the option
+   */
+  text: string;
+  /**
+   *  Indicates the option is selected
+   */
+  selected?: boolean;
+}
+
+export default function QuizOption({
+  order,
+  text,
+  selected = false,
+}: QuizOptionProps) {
   return (
-    <View>
-      <Text>QuizOption</Text>
-    </View>
+    <Pressable style={styles.option}>
+      <Text style={styles.optionNumber}>{order}</Text>
+      <Divider />
+      <View>
+        <Text style={styles.optionTitle}>{capitalize(text)}</Text>
+      </View>
+    </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  option: {
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 4,
+    backgroundColor: '#F2F2F2',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 6,
+  },
+  optionTitle: {
+    fontWeight: 'bold',
+    marginLeft: 12,
+  },
+  optionNumber: {
+    marginRight: 12,
+    fontWeight: 'bold',
+  },
+});
