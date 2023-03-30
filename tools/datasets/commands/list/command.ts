cbc596c60dae7d93059ebde71649e999723eeb7d
Peter <github@peter.dev.br>
2023-03-30 20:30:23 -0300

    refactor(tools): table cli header

--- tools/datasets/commands/list/command.ts
+++ tools/datasets/commands/list/command.ts
@@ -16,23 +16,18 @@ export const handler = async () => {
   let i = 0;
 
   console.log(
-    util.format(
-      '%s  %s  %s',
-      'Arquivo'.padEnd(30),
-      'Date'.padEnd(20),
-      'Size'.padEnd(20)
-    )
+    util.format('%s  %s  %s', 'Date'.padEnd(20), 'Size'.padEnd(10), 'Dataset')
   );
 
   for await (const stat of files.map((f) => fs.stat(path.join(dir, f)))) {
     console.log(
       '%s  %s  %s',
-      files[i].padEnd(30),
       DateTime.fromMillis(stat.birthtimeMs)
         .toLocal()
         .toFormat('dd-MM-yyyy HH:mm:ss')
         .padEnd(20),
-      bytes(stat.size).padEnd(20)
+      bytes(stat.size).padEnd(10),
+      files[i]
     );
 
     i++;
