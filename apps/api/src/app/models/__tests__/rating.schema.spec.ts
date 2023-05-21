80f65b9e5d8679ba86dcb6069ba9288ae973a700
Peter <github@peter.dev.br>
2023-05-21 12:41:03 -0300

    test(api): improove rating schema unit tests

--- apps/api/src/app/models/__tests__/rating.schema.spec.ts
+++ apps/api/src/app/models/__tests__/rating.schema.spec.ts
@@ -31,25 +31,46 @@ describe('RatingSchema', () => {
   it('invalid when rating is 0', async () => {
     doc.rating = 0;
 
-    await expect(doc.validate()).rejects.toMatchInlineSnapshot(
-      `[ValidationError: Rating validation failed: rating: Cast to Rating failed for value "0" (type number) at path "rating"]`
-    );
+    await expect(doc.validate()).rejects.toMatchObject({
+      errors: {
+        rating: {
+          name: 'CastError',
+          reason: {
+            message: 'Rating: value must be between 1 and 5',
+          },
+        },
+      },
+    });
   });
 
   it('invalid when rating is 6', async () => {
     doc.rating = 6;
 
-    await expect(doc.validate()).rejects.toMatchInlineSnapshot(
-      `[ValidationError: Rating validation failed: rating: Cast to Rating failed for value "6" (type number) at path "rating"]`
-    );
+    await expect(doc.validate()).rejects.toMatchObject({
+      errors: {
+        rating: {
+          name: 'CastError',
+          reason: {
+            message: 'Rating: value must be between 1 and 5',
+          },
+        },
+      },
+    });
   });
 
   it('invalid when not a number', async () => {
     doc.rating = 'invalid' as any;
 
-    await expect(() => doc.validate()).rejects.toMatchInlineSnapshot(
-      `[ValidationError: Rating validation failed: rating: Cast to Rating failed for value "invalid" (type string) at path "rating"]`
-    );
+    await expect(() => doc.validate()).rejects.toMatchObject({
+      errors: {
+        rating: {
+          name: 'CastError',
+          reason: {
+            message: 'Rating: value must be a number got invalid',
+          },
+        },
+      },
+    });
   });
 
   it('valid when nullish', async () => {
